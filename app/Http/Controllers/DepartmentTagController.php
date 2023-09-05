f8031850cfa0fa2ac14bb8008f13a3521dd54e53
dimahmz <essaadihamza4@outlook.com>
2023-09-05 01:40:14 +0100

    Tables Controllers

---
+++ app/Http/Controllers/DepartmentTagController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DepartmentTagController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/PropertyItemsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PropertyItemsController extends Controller
+{
+    //
+}
