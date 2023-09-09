79c954372fdaaf94e94bc6d89edd3173b0310d6b
dimahmz <essaadihamza4@outlook.com>
2023-09-09 02:47:15 +0100

    APIs end points

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,82 @@
 <?php
 
-use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Models\Orders\Order;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Food\FoodController;
+use App\Http\Controllers\Order\OrderController;
+use App\Http\Controllers\Food\PropertyController;
+use App\Http\Controllers\Food\FoodGroupController;
+use App\Http\Controllers\Food\VariationController;
+use App\Http\Controllers\Stock\PurchaseController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Dashboards\AdminDashboardController;
-use App\Http\Controllers\Dashboards\DeliverymanDashboardController;
 use App\Http\Controllers\Dashboards\StaffDashboardController;
+use App\Http\Controllers\Dashboards\DeliverymanDashboardController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return HttpResponses::success($request->user(), "user data", 200);
 });
 
-// Public Routes 
-Route::post('/test', function () {
-    return "api is working ";
+// --------  Public Routes  ------------
+Route::post('/test', function (Request $request) {
+    return "working";
 });
 
+// Auth Routes 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [AuthController::class, 'register']);
 
-// \Illuminate\Support\Facades\Log::info("log something");
+        // ------ Food -------
 
-// Protected Routes
+// food Routes 
+Route::get('/foods', [FoodController::class, 'get']);
+Route::post('/food/create', [FoodController::class, 'post']);
+Route::put('/food/update', [FoodController::class, 'put']);
+Route::delete('/food/delete', [FoodController::class, 'delete']);
+// food groups
+Route::get('/food/groups', [FoodGroupController::class, 'get']);
+Route::post('/food/groups/create', [FoodGroupController::class, 'post']);
+Route::put('/food/groups/update', [FoodGroupController::class, 'put']);
+Route::delete('/food/groups/delete', [FoodGroupController::class, 'delete']);
+// food properties
+Route::get('/food/properties', [PropertyController::class, 'get']);
+Route::post('/food/properties/create', [PropertyController::class, 'post']);
+Route::put('/food/properties/update', [PropertyController::class, 'put']);
+Route::delete('/food/properties/delete', [PropertyController::class, 'delete']);
+// food variations
+Route::get('/food/variations', [VariationController::class, 'get']);
+Route::post('/food/variations/create', [VariationController::class, 'post']);
+Route::put('/food/variations/update', [VariationController::class, 'put']);
+Route::delete('/food/variations/delete', [VariationController::class, 'delete']);
+
+    // ------- Orders ----------
+// Online
+Route::get('/orders', [OrderController::class, 'get']);
+Route::post('/order/create', [OrderController::class, 'post']);
+Route::put('/order/update', [OrderController::class, 'put']);
+Route::delete('/order/delete', [OrderController::class, 'delete']);
+
+// point of sell
+Route::get('/online_orders', [OnlineOrderController::class, 'get']);
+Route::post('/online_order/create', [OnlineOrderController::class, 'post']);
+Route::put('/online_order/update', [OnlineOrderController::class, 'put']);
+Route::delete('/online_order/order/delete', [OnlineOrderController::class, 'delete']);
+
+    
+
+
+
+
+// Purchase Routes
+Route::get('/purchases' , [PurchaseController::class, 'get']);
+Route::post('/purchases/create' , [PurchaseController::class, 'post']);
+
+
+// ------------ Protected Routes --------------
 Route::group(
     ['middleware' => ['auth:sanctum']],
 
@@ -59,21 +112,7 @@ Route::namespace(PasswordResetLinkController::class)->prefix('reset-password')->
 });
 
 
-// TODO an alternative for using just one route
-// Route::get('/dashboard', function () {
-//     // ...
-// })->middleware([First::class, Second::class]);
-
 // Catch-out any route that is not defined
 Route::any('{any}', function () {
     return HttpResponses::error(null, "The requested resource could not be found.", 404);
 })->where('any', '.*');
-
-//
-use App\Http\Controllers\API\CategoryController;
-use App\Http\Controllers\API\ArticleController;
-use App\Http\Controllers\API\TableController;
-
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/tables', [TableController::class, 'index']);
